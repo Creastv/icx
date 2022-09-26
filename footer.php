03b1d21ab240514c12d502fa965872dad59814e5
Creastv <piotr@roial.pl>
2022-09-26 10:59:08 +0200

    footer

--- footer.php
+++ footer.php
@@ -2,20 +2,24 @@
 </main>
  <footer id="footer" itemscope itemtype="http://schema.org/WPFooter">
         <div class="container">
-            <div class="row row-flex">
-                <div class="col-2">
-                    <div class="row row-flex">
+            <div class="row-left row-flex left-right">
+                <div class="col-2 left">
+                    <div class="row-flex">
                         <div class="col-2">
-                            <?php do_action( 'before_sidebar' ); ?>
-                            <?php if ( ! dynamic_sidebar( 'footer-1' ) ) : ?><?php endif; ?>
+                            <div class="wrap-footer-inf">
+                                <?php do_action( 'before_sidebar' ); ?>
+                                <?php if ( ! dynamic_sidebar( 'footer-1' ) ) : ?><?php endif; ?>
+                            </div>
                         </div>
                         <div class="col-2">
-                            <?php do_action( 'before_sidebar' ); ?>
-                            <?php if ( ! dynamic_sidebar( 'footer-2' ) ) : ?><?php endif; ?>
+                            <div class="wrap-footer-inf">
+                                <?php do_action( 'before_sidebar' ); ?>
+                                <?php if ( ! dynamic_sidebar( 'footer-2' ) ) : ?><?php endif; ?>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
+                <div class="col-2 right">
                     <?php echo do_shortcode('[contact-form-7 id="14" title="Formularz 1"]'); ?>
                 </div>
             </div>
